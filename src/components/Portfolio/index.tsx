2252fbebf7f335b6ff3cc7f33e8ed5159d734cf1
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2023-01-29 17:39:26 +0530

    portfolio stats

--- src/components/Portfolio/index.tsx
+++ src/components/Portfolio/index.tsx
@@ -1,17 +1,58 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { CoinContext, CoinContextType } from "../../context/CoinContext";
 import MarketStatComponent from "../common/MarketStatComponent";
 import Table from "../common/Table";
 import "./styles.css";
 
 function Portfolio() {
+  const [value, setValue] = useState<number>(0);
+  const [percentageChange, setPercentageChange] = useState<number>(0);
   const { portfolioCoins } = useContext(CoinContext) as CoinContextType;
+
+  const getPortfolioValue = () => {
+    const portfolioValue = portfolioCoins.reduce(
+      (prev, curr) => prev + (curr?.currentHoldings ?? 0 * curr.current_price),
+      0
+    );
+
+    const prevValue = portfolioCoins
+      .map((e) => {
+        const current: number = (e?.currentHoldings ?? 0) * e.current_price;
+        const percentChange: number =
+          (e?.price_change_percentage_24h ?? 0) / 100;
+
+        const prevValue: number = current / (1 + percentChange);
+        return prevValue;
+      })
+      .reduce((prev, curr) => prev + curr, 0);
+
+    const percentage =
+      ((portfolioValue - prevValue ?? 0) / (prevValue != 0 ? prevValue : 1)) *
+        100 ?? 0;
+    setPercentageChange(percentage);
+    setValue(portfolioValue);
+  };
+
+  useEffect(() => {
+    if (portfolioCoins) getPortfolioValue();
+  }, [portfolioCoins]);
 
   return (
     <div className="portfolio">
       <div className="title">Portfolio</div>
 
-      <MarketStatComponent title="Market Cap" body={4240.04} subtext={-2.03} />
+      <div className="row">
+        <MarketStatComponent
+          title="Portfolio Value"
+          body={value.toCurrency()}
+          subtext={percentageChange}
+        />
+
+        <MarketStatComponent
+          title="Total Holdings"
+          body={portfolioCoins.length.toString()}
+        />
+      </div>
 
       <div className="portfolioCoinsList">
         <Table isPortfolio={true} coins={portfolioCoins} />
