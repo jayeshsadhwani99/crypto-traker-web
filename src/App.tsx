d914280374159408296b040798d2fc7f30cf24a8
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2023-01-29 20:50:40 +0530

    error page

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,41 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import Portfolio from "./components/Portfolio";
 import RootData from "./components/RootData";
 import { GlobalProvider } from "./context/GlobalContext";
 import Root from "./pages";
 import About from "./pages/about";
 import Coin, { coinLoader } from "./pages/coin";
+import ErrorPage from "./pages/error";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "/",
         element: <RootData />,
+        errorElement: <ErrorPage />,
       },
       {
         path: ":coinId",
         element: <Coin />,
         loader: coinLoader,
+        errorElement: <ErrorPage />,
       },
     ],
   },
   {
     path: "about",
     element: <About />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "portfolio",
+    element: <Portfolio />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
